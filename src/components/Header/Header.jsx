ba87e7d089e92292bda5f8751512b66b0c7fd4ee
sharma01ketan <sharma01ketan@gmail.com>
2024-07-21 01:49:54 +0530

    shinChan centered

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -134,6 +134,91 @@
 
 
 
+// import React from 'react'
+// import {Container, Logo, LogoutBtn} from '../index'
+// import { Link } from 'react-router-dom'
+// import {useSelector} from 'react-redux'
+// import { useNavigate } from 'react-router-dom'
+
+// function Header() {
+//   const authStatus = useSelector((state) => state.auth.status)
+//   const navigate = useNavigate()
+
+//   const navItems = [
+//     {
+//       name: 'Home',
+//       slug: "/",
+//       active: true
+//     }, 
+//     {
+//       name: "Login",
+//       slug: "/login",
+//       active: !authStatus,
+//   },
+//   {
+//       name: "Signup",
+//       slug: "/signup",
+//       active: !authStatus,
+//   },
+//   {
+//       name: "All Posts",
+//       slug: "/all-posts",
+//       active: authStatus,
+//   },
+//   {
+//       name: "Add Post",
+//       slug: "/add-post",
+//       active: authStatus,
+//   },
+//   ]
+
+
+//   return (
+//     <header className='py-3 shadow bg-white w-full'>
+//       <Container>
+//         <nav className='flex flex-wrap items-center'>
+//           <div className='mr-4'>
+//             <Link to='/'>
+//               {/* <Logo width='70px'   /> */}
+//               <div style={{ display: 'flex', alignItems: 'center' }}>
+//                   <img src="/vite.svg" alt="Vite Logo" style={{ width: 70 }} />
+//                   <span style={{ marginLeft: '10px' }}>Let's Blog</span>
+//                   {/* <Logo width='70px'/> */}
+//               </div>
+//             </Link>
+//           </div>
+//           <ul className='flex flex-wrap ml-auto items-center'>
+//             {navItems.map((item) => 
+//             item.active ? (
+//               <li key={item.name} className="m-1">
+//                 <button
+//                 onClick={() => navigate(item.slug)}
+//                 className='inline-block px-6 py-2 m-[1px] duration-200 
+//                 hover:rounded-2xl
+//                 hover:text-black
+//                 border-[1px] border-transparent
+//                 hover:border-black
+//                 hover:bg-yellow-50
+//                 font-sans'
+//                 >{item.name}</button>
+//               </li>
+//             ) : null
+//             )}
+//             {authStatus && (
+//               <li>
+//                 <LogoutBtn />
+//               </li>
+//             )}
+//           </ul>
+//         </nav>
+//       </Container>
+//     </header>
+//   )
+// }
+
+// export default Header
+
+
 import React from 'react'
 import {Container, Logo, LogoutBtn} from '../index'
 import { Link } from 'react-router-dom'
@@ -154,40 +239,37 @@ function Header() {
       name: "Login",
       slug: "/login",
       active: !authStatus,
-  },
-  {
+    },
+    {
       name: "Signup",
       slug: "/signup",
       active: !authStatus,
-  },
-  {
+    },
+    {
       name: "All Posts",
       slug: "/all-posts",
       active: authStatus,
-  },
-  {
+    },
+    {
       name: "Add Post",
       slug: "/add-post",
       active: authStatus,
-  },
+    },
   ]
-
 
   return (
     <header className='py-3 shadow bg-white w-full'>
       <Container>
-        <nav className='flex flex-wrap items-center'>
-          <div className='mr-4'>
+        <nav className='flex flex-wrap items-center justify-between'>
+          <div className='flex justify-center w-full md:w-auto md:mr-4'>
             <Link to='/'>
-              {/* <Logo width='70px'   /> */}
-              <div style={{ display: 'flex', alignItems: 'center' }}>
-                  <img src="/vite.svg" alt="Vite Logo" style={{ width: 70 }} />
-  <span style={{ marginLeft: '10px' }}>Let's Blog</span>
-  {/* <Logo width='70px'/> */}
-</div>
+              <div className="flex items-center">
+                <img src="/vite.svg" alt="Vite Logo" className="w-[70px]" />
+                <span className="ml-2">Let's Blog</span>
+              </div>
             </Link>
           </div>
-          <ul className='flex flex-wrap ml-auto items-center'>
+          <ul className='flex flex-wrap justify-center w-full md:w-auto md:ml-auto items-center mt-4 md:mt-0'>
             {navItems.map((item) => 
             item.active ? (
               <li key={item.name} className="m-1">
